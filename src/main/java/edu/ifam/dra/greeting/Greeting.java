aeda5da4d7150e356abe143ef92767c44ef2b4d5
Jpmarinw <joaopedrosouzamarinho@gmail.com>
2026-03-26 17:26:27 -0400

    docs: atualiza javadoc de Greeting

--- src/main/java/edu/ifam/dra/greeting/Greeting.java
+++ src/main/java/edu/ifam/dra/greeting/Greeting.java
@@ -1,7 +1,7 @@
 package edu.ifam.dra.greeting;
 
 /**
- * Record que representa os dados da mensagem de saudação.
+ * Record genérico que representa os dados da mensagem de saudação.
  * Records gerenciam automaticamente os construtores, getters (nome do campo),
  * equals, hashCode e toString.
  */
